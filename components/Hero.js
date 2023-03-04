c8e379ac1af7b28e9869e42e739ca904f0e0e84b
UdayKiranDamodara <udaykirandamodara@gmail.com>
2023-03-04 13:19:06 +0530

    initial commit

---
+++ components/Hero.js
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import React from 'react'
+import heroImage from '../public/images/website/hero-bg.jpg'
+
+const Hero = () => {
+  return (
+    <section>
+      <div className='image-container'></div>
+      <h1>Hi, I'm Uday</h1>
+      <p>Some description about me. Lets change it check change1</p>
+    </section>
+  )
+}
+
+export default Hero
